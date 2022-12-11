2ebf15c633f48d253d16761bc643c7535abf4add
Nishant2323 <116739942+Nishant2323@users.noreply.github.com>
2022-12-11 08:44:19 +0530

    Time: 2 ms (16.80%), Space: 47.9 MB (35.86%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -13,26 +13,123 @@
  *     }
  * }
  */
+// class Solution {
+//     public int size(TreeNode root){
+//         if(root==null) return 0;
+//         int left= size(root.left);
+//         int rigth= size(root.right);
+//         return 1+left+rigth;
+//     }
+//     public int maxPathSum(TreeNode root) {
+//         if(root==null) return 0;
+//         if(size(root)==1)return root.val;
+//         int[]  max= new int[1];
+//         max[0]= Integer.MIN_VALUE;
+//          int i= sum(root,max);
+//         return max[0];
+//     }
+//     public int sum(TreeNode root,int[] max){
+//         if(root==null) return 0;
+//         int left= Math.max(sum(root.left,max),0);
+//         int right= Math.max(sum(root.right,max),0);
+//         max[0]= Math.max(max[0],(left+right+root.val));
+//         return root.val+Math.max(left,right);
+//     }
+// }
+
+
+
+
+
+
+
+
 class Solution {
-    public int size(TreeNode root){
-        if(root==null) return 0;
-        int left= size(root.left);
-        int rigth= size(root.right);
-        return 1+left+rigth;
-    }
+    int max =Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        if(root==null) return 0;
-        if(size(root)==1)return root.val;
-        int[]  max= new int[1];
-        max[0]= Integer.MIN_VALUE;
-         int i= sum(root,max);
-        return max[0];
+        int res= find(root);
+        return max;
     }
-    public int sum(TreeNode root,int[] max){
+    public int find(TreeNode root){
         if(root==null) return 0;
-        int left= Math.max(sum(root.left,max),0);
-        int right= Math.max(sum(root.right,max),0);
-        max[0]= Math.max(max[0],(left+right+root.val));
-        return root.val+Math.max(left,right);
+        int left= find(root.left);
+        int right = find(root.right);
+        if(left<0){
+            left=0;
+        }
+        if(right<0){
+            right=0;
+        }
+        max= Math.max(max, left+right+root.val);
+        return Math.max(right,left) +root.val;
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
